b39a4f6096386f40b95f869b44628e4ac21d40c7
Nicole <149076337+ninizinha@users.noreply.github.com>
2024-05-23 10:38:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ aplicacao.post('/', (req, res) => {
     res.send("Meu backend funcionando com método POST");
 });
 
+aplicacao.get('/moedas', (req, res) => {
+    const moedas = {
+        BRL: "real",
+        USD: "dolar",
+        EUR: "euro"
+    }
+    res.status(200).json(moedas);
+});
+
+aplicacao.get('/conversao/:moedas', (req, res) => {
+
+    let moedas = req.params.moedas.split("-");
+
+    let moeda1 = moedas[0];
+    let moeda2 = moedas[1];
+
+    console.log(moeda1);
+    console.log(moeda2);
+
+    const resultado = {
+        moedaOrigem: moeda1,
+        moedaDestino: moeda2
+    }
+
+    res.status(200).json(moedas);
+});
+
+
 aplicacao.listen(4000, () => {
     console.log("Estou batendo na porta 4000");
 })
